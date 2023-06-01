bc46ddc722555725a19811d06c26db1e58dfd3eb
Ivan Wu <bocksheng961136@gmail.com>
2023-06-01 16:16:55 +0800

    update coding

--- password.py
+++ password.py
@@ -2,10 +2,12 @@ defaultpw = 'a123456'
 count = 3
 
 while count > 0:
+	count = count - 1
 	password = input('請輸入密碼：')
 	if password == defaultpw:
 		print('登入成功')
 		break
 	else:
-		count = count - 1
-		print('密碼錯誤！還有', count, '次機會')
+		print('密碼錯誤！')
+		if count > 0:
+			print('還有', count, '次機會')
